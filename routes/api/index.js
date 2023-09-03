838a04c907047ac992d60280a7ceaf5e94075153
Michie <contact@michiewillman.com>
2023-09-03 14:49:35 -0700

    update route index

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const sliceRoutes = require("./slice-routes");
 
-// router.use("/videos", videoRoutes);
-// router.use("/users", userRoutes);
+router.use("/slices", sliceRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
