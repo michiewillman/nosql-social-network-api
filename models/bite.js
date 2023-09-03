abe858789f0c47d92e33620d44005c8a14835ae2
Michie <contact@michiewillman.com>
2023-09-03 14:46:13 -0700

    Export biteSchema

--- models/bite.js
+++ models/bite.js
@@ -1,23 +1,33 @@
-const biteSchema = new mongoose.Schema({
-  biteId: {
-    type: Schema.Types.ObjectId,
-    // set to a new id on default
-    default: () => new Types.ObjectId(),
+const { Schema, Types } = require("mongoose");
+
+const biteSchema = new mongoose.Schema(
+  {
+    biteId: {
+      type: Schema.Types.ObjectId,
+      // set to a new id on default
+      default: () => new Types.ObjectId(),
+    },
+    biteBody: {
+      type: String,
+      required: true,
+      maxLength: [280, "Cannot be more than 280 characters"],
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => formatDate(timestamp),
+    },
   },
-  biteBody: {
-    type: String,
-    required: true,
-    maxLength: [280, "Cannot be more than 280 characters"],
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => formatDate(timestamp),
-  },
-});
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 module.exports = biteSchema;
