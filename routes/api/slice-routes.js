910f58194134eadbf18c89c8d0aea90f39b22a33
Michie <contact@michiewillman.com>
2023-09-02 22:06:25 -0700

    Add slice routes

--- routes/api/slice-routes.js
+++ routes/api/slice-routes.js
@@ -1,20 +1,19 @@
 const router = require("express").Router();
 
 // /api/slices
-router.route("/").get(getAllSlices);
+router.route("/").get(getAllSlices).post(createSlice);
 
 // /api/slices/:sliceId
+router
+  .route("/:sliceId")
+  .get(getOneSlice)
+  .post(createSlice)
+  .put(updateSlice)
+  .delete(deleteSlice);
 
-// GET
-
-// POST
-
-// PUT
-
-// DELETE
-
-// /api/slices/:sliceId/reactions
-
-// POST
-
-// DELETE
+// /api/slices/:sliceId/bites
+router
+  .route("/:sliceId/bites")
+  .get(getOneSlice)
+  .post(createBite)
+  .delete(deleteBite);
