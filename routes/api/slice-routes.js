0e1b9243ec8ed405f981437d95d20c84dbe8a08e
Michie <contact@michiewillman.com>
2023-09-11 17:46:38 -1000

    Import slice functions to routes

--- routes/api/slice-routes.js
+++ routes/api/slice-routes.js
@@ -1,4 +1,11 @@
 const router = require("express").Router();
+const {
+  getAllSlices,
+  getOneSlice,
+  createSlice,
+  updateSlice,
+  deleteSlice,
+} = require("../../controllers/slice-controller");
 
 // /api/slices
 router.route("/").get(getAllSlices).post(createSlice);
